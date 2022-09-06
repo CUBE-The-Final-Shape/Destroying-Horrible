e777add4e3abeb69044552629fd31017b425a4c9
CUBE <gustav.thn.karlsson@gmail.com>
2022-09-06 16:25:21 +0200

    Gun Fixed
    Gun
    + Fire rate limit
    + No gravity on bullets
    + Bullets unloaded on impact
    + Bullets spawn at barrel end
    
    - LAG

--- Scenes/gun_script.gd
+++ Scenes/gun_script.gd
@@ -1,15 +1,20 @@
 extends KinematicBody2D
 
-export var bullet_speed = 100
+export var bullet_speed = 200
+export var fire_rate = 0.2
 
 var bullet = preload("res://Scenes/Bullet.tscn")
+var can_fire = true
 
 func _process(delta):
 	look_at(get_global_mouse_position())
 
-	if Input.is_action_pressed("fire"):
+	if Input.is_action_pressed("fire") and can_fire:
 		var bullet_instance = bullet.instance()
-		bullet_instance.position = get_global_position()
+		bullet_instance.position = $BulletPoint.get_global_position()
 		bullet_instance.rotation_degrees = rotation_degrees
 		bullet_instance.apply_impulse(Vector2(), Vector2(bullet_speed, 0).rotated(rotation))
 		get_tree().get_root().add_child(bullet_instance)
+		can_fire = false
+		yield(get_tree().create_timer(fire_rate), "timeout")
+		can_fire = true
