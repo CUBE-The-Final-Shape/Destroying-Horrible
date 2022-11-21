2a7b39e6c3293f1d0080d16c0456a91632623fe2
CUBE-The-Final-Shape <gustav.thn.karlsson@gmail.com>
2022-11-21 15:16:47 +0100

    Small fixes + transition
    transition not implemented

--- Scripts/Camera_anchor.gd
+++ Scripts/Camera_anchor.gd
@@ -1,11 +1,17 @@
 extends RemoteTransform2D
 
 func _process(_delta):
-	if Input.is_action_pressed("ui_right"):
-		position = Vector2(40, -20)
-	elif Input.is_action_pressed("ui_left"):
-		position = Vector2(-40, -20)
-	elif Input.is_action_pressed("crouch"):
-		position = Vector2(0, 10)
+
+	if Input.is_action_pressed("crouch"):
+		position = Vector2(0, 5)
+		if Input.is_action_pressed("ui_right"):
+			position = Vector2(40, 5)
+		elif Input.is_action_pressed("ui_left"):
+			position = Vector2(-40, 5)	
 	else:
-		position = Vector2(0, -20)
+		if Input.is_action_pressed("ui_right"):
+			position = Vector2(40, -20)
+		elif Input.is_action_pressed("ui_left"):
+			position = Vector2(-40, -20)
+		else:
+			position = Vector2(0, -20)
