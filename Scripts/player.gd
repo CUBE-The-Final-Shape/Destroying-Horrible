c0cf2cfff57bb67687cb6468fa0b5ebd09087ad8
CUBE <gustav.thn.karlsson@gmail.com>
2023-04-05 16:20:29 +0200

    It cool now

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,8 @@ export (int) var gravity = 700
 export (int) var max_health = 100
 const UP_DIRECTION = Vector2.UP
 onready var health = max_health setget _set_health
+onready var _animated_sprite = $AnimatedSprite
+onready var right = 1
 
 var velocity = Vector2()
 
@@ -18,12 +20,10 @@ func _physics_process(delta):
 		Input.get_action_strength("right")
 		- Input.get_action_strength("left")
 	)
-	
 	velocity.x = horizontal_direction * speed
 	velocity.y += gravity * delta
 	
 	var is_jumping := Input.is_action_just_pressed("up") and is_on_floor()
-	
 	var is_crouching := Input.is_action_pressed("crouch")
 	
 	if is_jumping:
@@ -62,48 +62,47 @@ func _set_health(value):
 
 # Animation code below. It is a mess
 
-# Animation code below. It is a mess
-onready var _animated_sprite = $AnimatedSprite
-
 func _process(_delta):
 	# Checks for cursor position. The data is used to always keep the player facing the cursor
 	var mpos = get_global_mouse_position()
 	var pos = global_position
 	var rot = rad2deg((mpos - pos).angle())
-	# This is honestly a mess of animation code but it makes sure that the correct animation is played for every action. 
-	# It checks for if the player is crouching or not and makes sure the player always faces their cursor
-	# Rotates the sprite to always face the camera (Exept when idle...)
-	if(rot >= -90 and rot <= 90):
-		if Input.is_action_pressed("ui_right"):	
-			$".".get_node("AnimatedSprite").flip_h = false
-		elif Input.is_action_pressed("ui_left"):
-			$".".get_node("AnimatedSprite").flip_h = true
-	else:
-		if Input.is_action_pressed("ui_right"):	
-			$".".get_node("AnimatedSprite").flip_h = true
-		elif Input.is_action_pressed("ui_left"):
-			$".".get_node("AnimatedSprite").flip_h = false
-	
+	# Collision code. Disables and enables the standing collision based on crouching 
 	if Input.is_action_pressed("crouch"):
 		get_node("Collision_Standing").disabled = true
 	else:
 		get_node("Collision_Standing").disabled = false
-	
-	_animated_sprite.play("idle_right")
-	
+	# This is honestly a mess of animation code but it makes sure that the correct animation is played for every action. 
+	# It checks for if the player is crouching or not and makes sure the player always faces their cursor
+	if (right == 1):
+		if (rot >= -90 and rot <= 90):
+			$".".get_node("AnimatedSprite").flip_h = false
+		else:
+			$".".get_node("AnimatedSprite").flip_h = true
+	else:
+		if (rot >= -90 and rot <= 90):
+			$".".get_node("AnimatedSprite").flip_h = true
+		else:
+			$".".get_node("AnimatedSprite").flip_h = false
 	# Somewhat optimized animation script
 	if Input.is_action_pressed("ui_right"):	
+		right = 1
 		if Input.is_action_pressed("crouch"):
 			_animated_sprite.play("crouch_right")
 		else:
 			_animated_sprite.play("walk_right")
 	elif Input.is_action_pressed("ui_left"):
+		right = 0
 		if Input.is_action_pressed("crouch"):
 			_animated_sprite.play("crouch_left")
 		else:
 			_animated_sprite.play("walk_left")
-			
-			if(rot >= -90 and rot <= 90):
-				_animated_sprite.play("idle_right")
-			else:
-				_animated_sprite.play("idle_left")
+	else:
+		if(right == 1):
+			_animated_sprite.play("idle_right")
+			if Input.is_action_pressed("crouch"):
+				_animated_sprite.play("crouch_idle_right")
+		else:
+			_animated_sprite.play("idle_left")
+			if Input.is_action_pressed("crouch"):
+				_animated_sprite.play("crouch_idle_left")
